eaaa0a2f0dc8c8a28705397d7230f3296949e747
Nabla <darksider96@gmail.com>
2019-09-26 07:11:47 +0200

    added tiles

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const config = {
-    cityCode: '3171727', // http://bulk.openweathermap.org/sample/ to find your city code
+    // cityCode: '3171727', // http://bulk.openweathermap.org/sample/ to find your city code
+    cityCode: '658226', // http://bulk.openweathermap.org/sample/ to find your city code
     tempMetric: "metric" // metric | imperial
 }
